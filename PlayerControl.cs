ab7d5c58e7a940c628954987b80190b3b6ac9d0a
Eduardo <eduardo.passeri@gmail.com>
2026-05-25 16:39:14 +0100

    Add files via upload
    Finalised version of the script for my working journal, still a long way to go for a finished product tho
    For some reason it's broken when you export it but in the editor it works just fine :/
    Also I still need to add mantling

--- PlayerControl.cs
+++ PlayerControl.cs
@@ -7,14 +7,24 @@ public class PlayerControl : MonoBehaviour
     public Transform playerObj;
     // public CharacterController CC; the character controller component isn't designed for 2D, will have to find a replacement
     public Transform mainCamera;
-    public float FOV;
+    public Collider2D capsCollider;
+    float FOV = 10;
     Vector3 movement = new Vector3 (0, 0, 0);
+    Vector3 velocity = new Vector3 (0, 0, 0);
     float speed;
     public float walkSpeed;
     public float runSpeed;
     public float crouchSpeed;
-    float fallSpeed = 9.81f; // not used yet cuz I still need to figure out 2D raycasts so I can add gravityyyyy
-    bool grounded;
+    public Vector2 a = new Vector2(1,1).normalized;
+    public Vector2 b;
+
+    float fallSpeed = 0.5f; 
+    public bool grounded;
+    public float jumpCooldown;
+    bool readyToJump;
+
+    RaycastHit2D groundCheckHit;
+    LayerMask ground;
 
     Vector2 ToVector2D(Vector3 vector) // converts 3D vectors to 2D
     {
@@ -28,45 +38,146 @@ public class PlayerControl : MonoBehaviour
         return output;
     }
 
+    bool OnSlope()
+    {
+        return (Vector2.up != groundCheckHit.normal);
+    }
+
+    Vector3 SlopeMove(Vector3 movementVector)
+    {
+        return Vector3.ProjectOnPlane(movementVector, ToVector3D(groundCheckHit.normal));
+    }
+
+    public MovementState playerState;
+
+    public enum MovementState
+    {
+        walking,
+        running,
+        crouching,
+        airborne
+            // add mantling later
+    }
+
+    void GroundSnap() // snaps player relative to groundcheck position if the player isn't aligned to the ground properly
+    {
+        Vector3 a = playerObj.position;
+        Vector3 b = ToVector3D(groundCheckHit.point);
+        if (a.y != b.y + 1)
+        {
+            Vector3 c = new Vector3(0, a.y - (b.y + 1), 0);
+            playerObj.position -= c;
+        }
+    }
+
     void GetInput()
     {
-        if (Input.GetKey("c")) // sets movement speed depending on movement state
+        if (Input.GetKey("c") && grounded) // sets movement speed depending on movement state
         {
             speed = crouchSpeed;
+            playerState = MovementState.crouching;
         }
-        else if (Input.GetKey("left shift"))
+        else if (Input.GetKey("left shift") && grounded)
         {
             speed = runSpeed;
+            playerState = MovementState.running;
         }
-        else
+        else if (grounded)
         {
             speed = walkSpeed;
+            playerState = MovementState.walking;
+        }
+        else if (!grounded)
+        {
+            playerState = MovementState.airborne;
         }
 
-        movement.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime; // ammend for jump arcs and falling later
+        if (grounded)
+        {
+            velocity.y = 0;
+            movement.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            if (Input.GetButtonDown("Jump") && readyToJump != false)
+            {
+                grounded = false;
+                velocity.y = 2 * Mathf.Sqrt(16f * fallSpeed);
+                readyToJump = false;
+                Invoke(nameof(ResetJump), jumpCooldown);
+            }
+
+            if (Input.GetAxisRaw("Horizontal") != movement.x && Input.GetAxisRaw("Horizontal") != 0)
+            {
+                playerObj.localScale = new Vector3(Input.GetAxisRaw("Horizontal"), 1, 1); // flips player to face left or right
+            }
+        }
+        else if (!grounded)
+        {
+            velocity.y -= fallSpeed * Time.deltaTime;
+        }
+    }
+
+    void GroundCheck()
+    {   // creates a raycheck from the position of the capsule collider
+        groundCheckHit = Physics2D.Raycast(ToVector2D(playerObj.position), Vector2.down, 1.1f, ground, -1f, 1f);
+        if (groundCheckHit != false && grounded == false)
+        {
+            if (velocity.y < 0)
+            {
+                movement.y = 0;
+            }
+            velocity.y = 0;
+            grounded = true;
+            GroundSnap();
+        }
+        else if (groundCheckHit == false && grounded != false)
+        {
+            grounded = false;
+        }
+    }
+
+    void ResetJump()
+    {
+        readyToJump = true;
     }
 
     void Move()
     {
-        if (Input.GetAxisRaw("Horizontal") != movement.x && Input.GetAxisRaw("Horizontal") != 0)
-        {
-            playerObj.localScale = new Vector3(Input.GetAxisRaw("Horizontal"), 1, 1); // flips player to face left or right
-        }
+        GroundCheck();
         GetInput();
-        if (movement.magnitude != 0) // updates movement only if the player isn't still
+        if (movement.magnitude != 0 || velocity.y != 0) // updates movement only if the player isn't still or has no momentum
         {
-            playerObj.position += movement; // changes player position based on movement value
-            mainCamera.position = playerObj.position + new Vector3(movement.x * 10, movement.y * 10, -FOV); // camera stays ahead of the player by how fast it travels
+            if (grounded && !OnSlope())
+            {
+                playerObj.position += movement;
+            }
+            else if(grounded && OnSlope())
+            {   // movement is snapped to slope as long as the player isn't jumping
+                if (readyToJump != false) 
+                {
+                    playerObj.position += SlopeMove(movement);
+                }
+                else
+                {
+                    playerObj.position += movement;
+                }
+            }
+            else if (!grounded)
+            {
+                movement += velocity * Time.deltaTime;
+                playerObj.position += movement;
+            }
         }
     }
 
     void Start()
     {
         mainCamera.position = playerObj.position + new Vector3(0, 0, -FOV); // snaps camera to player on start
+        ground = LayerMask.GetMask("Ground");
+        readyToJump = true;
     }
 
     void Update()
     {
         Move();
+        mainCamera.position = playerObj.position + new Vector3(movement.x * 10, movement.y * 10, -FOV);
     }
 }

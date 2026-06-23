c15bf7207c021bd12e69c869130444fbcbcafd69
Ryan Harvey <rkharvey@mekapaedia.com>
2026-06-24 00:00:16 +0100

    Basic movement

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -3,20 +3,81 @@ using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    Rigidbody2D rigid_body;
+    float move_force = 15f;
+    float jump_force = 30f;
+    float drag = .25f;
+    Vector2 cur_move_vec;
+    bool jumping = false;
+    float jump_length = 2f;
+    float jump_start = 0f;
+
     void Start()
     {
-        
+        rigid_body = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
+    void FixedUpdate()
+    {
+        rigid_body.AddForceX(-Mathf.Sign(rigid_body.linearVelocityX) * Mathf.Pow(rigid_body.linearVelocityX, 2) * drag);
+        rigid_body.AddForceX(cur_move_vec.x * move_force);
+
+        if (cur_move_vec.y > 0)
+        {
+            if (!jumping)
+            {
+                ContactPoint2D[] contacts = new ContactPoint2D[rigid_body.attachedColliderCount];
+                rigid_body.GetContacts(contacts);
+                foreach (var contact in contacts)
+                {
+                    print($"{contact.normal.x}, {contact.normal.y}");
+                    if (contact.normal.y == 1)
+                    {
+                        jumping = true;
+                        jump_start = Time.fixedTime;
+                        break;
+                    }
+                }
+            }
+        }
+        else if (jumping)
+        {
+            jumping = false;
+        }
+
+        if (jumping)
+        {
+            float jump_time = Time.fixedTime - jump_start;
+            if (jump_time >= jump_length)
+                jumping = false;
+            if (jumping)
+            {
+                float jump_resist = Mathf.Pow((jump_time / jump_length) * jump_force, 2);
+                if (jump_resist < jump_force)
+                    rigid_body.AddForceY(jump_force - jump_resist);
+                else
+                    jumping = false;
+            }
+        }
+    }
+
     public void OnMove(InputValue value)
     {
-        print("On move");
+        cur_move_vec = value.Get<Vector2>();
     }
+
+    public void OnAttack(InputValue value)
+    {
+    }
+
+    public void OnInteract(InputValue value)
+    {
+    }
+
 }

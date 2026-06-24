b227b2d607ab0919176f38efd070e0be15152470
Ryan Harvey <rkharvey@mekapaedia.com>
2026-06-24 02:06:26 +0100

    Add animations

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -1,26 +1,64 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.U2D.Animation;
+
+enum PlayerState
+{
+    Idle,
+    Attack,
+    Crouch_Down,
+    Crouch_Up,
+    Damaged,
+    Death,
+    Jump,
+    Run,
+    Sneak,
+    Turn
+};
 
 public class PlayerControl : MonoBehaviour
 {
+    public Sprite[] idle_frames;
+    public Sprite[] run_frames;
 
     Rigidbody2D rigid_body;
+    SpriteRenderer sprite_renderer;
     float move_force = 15f;
-    float jump_force = 30f;
+    float jump_force = 40f;
     float drag = .25f;
     Vector2 cur_move_vec;
     bool jumping = false;
-    float jump_length = 2f;
+    float jump_length = 1.7f;
     float jump_start = 0f;
+    int cur_frame = 0;
+    float last_frame_time = 0f;
+    float idle_frame_time = 1f / 6f;
+    float run_frame_time = 1f / 11f;
+    bool flipped = false;
+    PlayerState state = PlayerState.Idle;
 
     void Start()
     {
         rigid_body = GetComponent<Rigidbody2D>();
+        sprite_renderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        
+        if (state == PlayerState.Idle && Time.fixedTime - last_frame_time >= idle_frame_time)
+        {
+            cur_frame = (cur_frame + 1) % idle_frames.Length;
+            last_frame_time = Time.fixedTime;
+            sprite_renderer.sprite = idle_frames[cur_frame];
+        }
+        else if (state == PlayerState.Run && Time.fixedTime - last_frame_time >= run_frame_time)
+        {
+            cur_frame = (cur_frame + 1) % run_frames.Length;
+            last_frame_time = Time.fixedTime;
+            sprite_renderer.sprite = run_frames[cur_frame];
+        }
+
+        sprite_renderer.flipX = flipped;
     }
 
     void FixedUpdate()
@@ -36,7 +74,6 @@ public class PlayerControl : MonoBehaviour
                 rigid_body.GetContacts(contacts);
                 foreach (var contact in contacts)
                 {
-                    print($"{contact.normal.x}, {contact.normal.y}");
                     if (contact.normal.y == 1)
                     {
                         jumping = true;
@@ -64,6 +101,17 @@ public class PlayerControl : MonoBehaviour
                 else
                     jumping = false;
             }
+        }
+
+        if (rigid_body.linearVelocityX != 0)
+        {
+            run_frame_time = 1f / (1.7f * Mathf.Abs(rigid_body.linearVelocityX));
+            state = PlayerState.Run;
+            flipped = rigid_body.linearVelocityX < 0;
+        }
+        else
+        {
+            state = PlayerState.Idle;
         }
     }
 
